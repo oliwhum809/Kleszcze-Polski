e9cfd63597fb7dc31e9e80feb031bbbfb04dbf8c
Oliwia <oliwhum809@student.polsl.pl>
2023-11-12 22:35:37 +0100

    Making scripts iniversal

--- adjust_images.py
+++ adjust_images.py
@@ -16,26 +16,26 @@ def generate_to_folder(number_of_images, generate_from, generate_to):
         horizontal_flip=True,
         fill_mode='nearest')
     while(counter < number_of_images):
-        for image_path in glob.glob(generate_from + '\\*'):
+        for image_path in glob.glob(generate_from + '/*'):
             img = load_img(image_path)
             x = img_to_array(img)
             x = x.reshape((1,) + x.shape)
             datagen.flow(x, 
                          batch_size=1,
                          save_to_dir = generate_to,
-                         save_prefix = image_path.split('\\')[-1].split('.')[-2] + '_augmented',
-                         save_format = image_path.split('\\')[-1].split('.')[-1]).next()
+                         save_prefix = image_path.split('/')[-1].split('.')[-2] + '_augmented',
+                         save_format = image_path.split('/')[-1].split('.')[-1]).next()
             counter = counter + 1
             if counter >= number_of_images: break
 
 def move_to_folder(number_of_images, move_from, move_to):
     counter = 0
     while(counter < number_of_images):
-        images_paths = glob.glob(move_from + '\\*')   
+        images_paths = glob.glob(move_from + '/*')   
         random.shuffle(images_paths)    
         image_path = images_paths[0]
-        image_name = image_path.split('\\')[-1]
-        new_image_path = move_to + '\\' + image_name
+        image_name = image_path.split('/')[-1]
+        new_image_path = move_to + '/' + image_name
         os.rename(image_path, new_image_path)
         counter = counter + 1
 
@@ -43,25 +43,29 @@ def move_to_folder(number_of_images, move_from, move_to):
 ###################################################################################################################
 
 args = sys.argv[1:]
-train_number = int(args[0])
-validation_number = int(args[1])
+data_set_name = args[0]
+train_number = int(args[1])
+validation_number = int(args[2])
+
+classes = [folder_path.split('/')[-1] for folder_path in glob.glob(os.getcwd() + '/' + data_set_name + '/*')]
+adjusted_data_set_name = 'adjusted_' + data_set_name
 
 try:
-    os.mkdir('adjusted_data_set')
+    os.mkdir(adjusted_data_set_name)
     for type in ['train', 'validate']:
-        os.mkdir('adjusted_data_set\\' + type)
-        for genom in ['Ixodes', 'Dermacentor', 'Amblyomma']:
-            os.mkdir('adjusted_data_set\\' + type + '\\' + genom)
+        os.mkdir(adjusted_data_set_name + '/' + type)
+        for class_name in classes:
+            os.mkdir(adjusted_data_set_name + '/' + type + '/' + class_name)
 except OSError as error:
     print('Skipping folder creation... They already exist!')
-for folder in glob.glob(os.getcwd() + '\\manipulated_data_set' + '\\*'):
-    train_folder = folder.split('\\')
-    train_folder[-2] = 'adjusted_data_set\\train'
-    train_folder = '\\'.join(train_folder)
-    validation_folder = folder.split('\\')
-    validation_folder[-2] = 'adjusted_data_set\\validate'
-    validation_folder = '\\'.join(validation_folder)
-    images_paths = glob.glob(folder + '\\*')
+for folder in glob.glob(os.getcwd() + '/' + data_set_name + '/*'):
+    train_folder = folder.split('/')
+    train_folder[-2] = adjusted_data_set_name + '/train'
+    train_folder = '/'.join(train_folder)
+    validation_folder = folder.split('/')
+    validation_folder[-2] = adjusted_data_set_name + '/validate'
+    validation_folder = '/'.join(validation_folder)
+    images_paths = glob.glob(folder + '/*')
     if (len(images_paths) > validation_number and len(images_paths) >= validation_number + train_number):
         move_to_folder(validation_number, folder, validation_folder)
         move_to_folder(train_number, folder, train_folder)
--- manipulate_images.py
+++ manipulate_images.py
@@ -1,17 +1,23 @@
-import os
+import os, sys
 from PIL import Image
 import glob
 
+args = sys.argv[1:]
+data_set_name = args[0]
+
+classes = [folder_path.split('/')[-1] for folder_path in glob.glob(os.getcwd() + '/' + data_set_name + '/*')]
+manipulated_data_set_name = 'manipulated_' + data_set_name
+
 try:
-    os.mkdir('manipulated_data_set')
-    for genom in ['Ixodes', 'Dermacentor', 'Amblyomma']:
-        os.mkdir('manipulated_data_set\\' + genom)
+    os.mkdir(manipulated_data_set_name)
+    for class_name in classes:
+        os.mkdir(manipulated_data_set_name + '/' + class_name)
 except OSError as error:
     print('Skipping folder creation... They already exist!')
-for folder in glob.glob(os.getcwd() + '\\Mini_Bejbikowy_Zestawik_Kleszczy' + '\\*'):
-    for image_path in  glob.glob(folder + '\\*'):
+for folder in glob.glob(os.getcwd() + '/' + data_set_name + '/*'):
+    for image_path in  glob.glob(folder + '/*'):
         image = Image.open(image_path)
         image = image.resize((320, 320))
-        new_image_path = image_path.split('\\')
-        new_image_path[-3] = 'manipulated_data_set'
-        image.save('\\'.join(new_image_path))
+        new_image_path = image_path.split('/')
+        new_image_path[-3] = manipulated_data_set_name
+        image.save('/'.join(new_image_path))
